5f720812f03c8f53019c8f730a5a112c3e839edb
Roar <rfo909@hotmail.com>
2020-09-18 10:53:04 +0200

    v1.4.0

--- src/rf/configtool/main/runtime/lib/text/ObjFrontEndAdHoc.java
+++ src/rf/configtool/main/runtime/lib/text/ObjFrontEndAdHoc.java
@@ -1,41 +1,95 @@
 package rf.configtool.main.runtime.lib.text;
 
+import java.util.HashMap;
+import java.util.List;
+
+import rf.configtool.main.Ctx;
 import rf.configtool.main.runtime.ColList;
+import rf.configtool.main.runtime.Function;
 import rf.configtool.main.runtime.Obj;
-import rf.configtool.main.runtime.lib.text.ObjText.FunctionLexer;
-import rf.configtool.main.runtime.lib.text.ObjText.FunctionParser;
+import rf.configtool.main.runtime.Value;
+import rf.configtool.main.runtime.ValueNull;
+import rf.configtool.main.runtime.ValueObj;
+import rf.configtool.parser.CharTable;
 
 public class ObjFrontEndAdHoc extends Obj implements FrontEnd {
-    
-    public ObjFrontEndAdHoc (String line) {
-//        this.add(new FunctionLexer());
-//        this.add(new FunctionParserDynamic());
-    }
-    
-    @Override
-    public boolean eq(Obj x) {
-        return x==this;
-    }
+
+	private String line;
+	private HashMap<String, CharTable> tokenTypes = new HashMap<String, CharTable>();
+
+	public ObjFrontEndAdHoc(String line) {
+		this.line=line;
+		this.add(new FunctionToken());
+	}
+
+	@Override
+	public boolean eq(Obj x) {
+		return x == this;
+	}
+
+	@Override
+	public String getTypeName() {
+		return "Parser.FrontEndAdHoc";
+	}
+
+	@Override
+	public ColList getContentDescription() {
+		return ColList.list().regular(getDesc());
+	}
+
+	private String getDesc() {
+		return getTypeName();
+	}
+
+	private Obj theObj() {
+		return this;
+	}
+
+	// ---------------------------------------
+	// FrontEnd interface
+	// ---------------------------------------
+
+	public FrontEndState getInputPos() {
+		return null;
+	}
+
+	public void setInputPos(FrontEndState mark) {
+		// todo
+	}
+
+	/**
+	 * Match token and return Value, or Java null if failing
+	 */
+	public Value matchToken(String tokenSelectionString) throws Exception {
+		return null;
+	}
+	
+	public String getSourceLocation() {
+		return "";
+	}
 
 
-    @Override
-    public String getTypeName() {
-        return "Parser.FrontEndAdHoc";
-    }
-    
+	// ------------------------------------
 
-    @Override
-    public ColList getContentDescription() {
-        return ColList.list().regular(getDesc());
-    }
+	class FunctionToken extends Function {
+		public String getName() {
+			return "Token";
+		}
 
-    
-    private String getDesc() {
-        return getTypeName();
-    }
-    
-    private Obj theObj () {
-        return this;
-    }
-    
+		public String getShortDesc() {
+			return "Token(name,lexerNode) - add token definition - returns self";
+		}
+
+		public Value callFunction(Ctx ctx, List<Value> params) throws Exception {
+			if (params.size() != 2)
+				throw new Exception("Expected parameters name, lexerNode");
+			String name = getString("name", params, 0);
+			ObjLexerNode node = (ObjLexerNode) getObj("lexerNode", params, 1);
+
+			tokenTypes.put(name, node.getCharTable());
+
+			return new ValueObj(theObj());
+		}
+	}
+
 }
--- src/rf/configtool/main/runtime/lib/text/ObjFrontEndRegular.java
+++ src/rf/configtool/main/runtime/lib/text/ObjFrontEndRegular.java
@@ -1,43 +1,96 @@
 package rf.configtool.main.runtime.lib.text;
 
+import java.util.HashMap;
 import java.util.List;
 
+import rf.configtool.main.Ctx;
 import rf.configtool.main.runtime.ColList;
+import rf.configtool.main.runtime.Function;
 import rf.configtool.main.runtime.Obj;
-import rf.configtool.main.runtime.lib.text.ObjText.FunctionLexer;
-import rf.configtool.main.runtime.lib.text.ObjText.FunctionParser;
+import rf.configtool.main.runtime.Value;
+import rf.configtool.main.runtime.ValueNull;
+import rf.configtool.main.runtime.ValueObj;
 
 public class ObjFrontEndRegular extends Obj implements FrontEnd {
-    
-    public ObjFrontEndRegular (ObjLexerTokenStream tokenStream) {
+
+	private ObjLexerTokenStream tokenStream;
+	private HashMap<String, Integer> tokenTypes = new HashMap<String, Integer>();
+
+	public ObjFrontEndRegular(ObjLexerTokenStream tokenStream) {
+		this.tokenStream=tokenStream;
+		this.add(new FunctionToken());
 //        this.add(new FunctionLexer());
 //        this.add(new FunctionParserDynamic());
-    }
-    
-    @Override
-    public boolean eq(Obj x) {
-        return x==this;
-    }
+	}
+
+	@Override
+	public boolean eq(Obj x) {
+		return x == this;
+	}
+
+	@Override
+	public String getTypeName() {
+		return "Parser.FrontEndRegular";
+	}
+
+	@Override
+	public ColList getContentDescription() {
+		return ColList.list().regular(getDesc());
+	}
+
+	private String getDesc() {
+		return getTypeName();
+	}
+
+	private Obj theObj() {
+		return this;
+	}
+
+	// ---------------------------------------
+	// FrontEnd interface
+	// ---------------------------------------
+
+	public FrontEndState getInputPos() {
+		return null;
+	}
+
+	public void setInputPos(FrontEndState mark) {
+		// todo
+	}
+
+	/**
+	 * Match token and return Value, or Java null if failing
+	 */
+	public Value matchToken(String tokenSelectionString) throws Exception {
+		return null;
+	}
+	
+	public String getSourceLocation() {
+		return "";
+	}
 
 
-    @Override
-    public String getTypeName() {
-        return "Parser.FrontEndRegular";
-    }
-    
+	// ---------------------------------------
 
-    @Override
-    public ColList getContentDescription() {
-        return ColList.list().regular(getDesc());
-    }
+	class FunctionToken extends Function {
+		public String getName() {
+			return "Token";
+		}
 
-    
-    private String getDesc() {
-        return getTypeName();
-    }
-    
-    private Obj theObj () {
-        return this;
-    }
-    
+		public String getShortDesc() {
+			return "Token(name,tokenType) - add token definition - returns self";
+		}
+
+		public Value callFunction(Ctx ctx, List<Value> params) throws Exception {
+			if (params.size() != 2)
+				throw new Exception("Expected parameters name, tokenType (int)");
+			String name = getString("name", params, 0);
+			Integer tokenType = (int) getInt("tokenType", params, 1);
+
+			tokenTypes.put(name, tokenType);
+
+			return new ValueObj(theObj());
+		}
+	}
+
 }
