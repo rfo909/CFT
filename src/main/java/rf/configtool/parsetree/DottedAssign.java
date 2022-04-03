dc0018cd66efa33a28753d0e899857ad63d5854a
Roar <rfo909@hotmail.com>
2022-04-03 22:41:17 +0200

    v3.3.0 improvements and tests

---
+++ src/main/java/rf/configtool/parsetree/DottedAssign.java
@@ -0,0 +1,70 @@
+/*
+CFT - an interactive programmable shell for automation 
+Copyright (C) 2020-2022 Roar Foshaug
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+package rf.configtool.parsetree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rf.configtool.lexer.TokenStream;
+import rf.configtool.main.Ctx;
+import rf.configtool.main.SourceException;
+import rf.configtool.main.runtime.Function;
+import rf.configtool.main.runtime.Obj;
+import rf.configtool.main.runtime.Value;
+import rf.configtool.main.runtime.ValueBoolean;
+import rf.configtool.main.runtime.ValueObj;
+import rf.configtool.main.runtime.lib.ObjClosure;
+import rf.configtool.main.runtime.lib.ObjDict;
+
+/**
+ * .x=Expr
+ */
+public class DottedAssign extends LexicalElement {
+
+    private String ident;
+    private Expr expr;
+    
+    public DottedAssign (TokenStream ts) throws Exception {
+        super(ts);
+
+        ts.matchStr(".","Expected .identifier=Expr");
+        ident=ts.matchIdentifier("expected .identifier");
+        ts.matchStr("=", "Expected .identifier=Expr");
+        expr=new Expr(ts);
+    }
+    
+    
+    
+    public Value resolve (Ctx ctx, Obj obj) throws Exception {
+        if (obj instanceof ValueObj) {
+        	// unwrap Obj
+            obj=((ValueObj) obj).getVal();
+        }
+        
+        if (obj instanceof ObjDict) {
+        	ObjDict dict=(ObjDict) obj;
+        	
+        	Value v=expr.resolve(ctx.sub());
+        	dict.set(ident, v);
+        	return new ValueObj(dict);
+        } else {
+        	throw new Exception("Expected a dictionary to save " + ident + " value into");
+        }
+    }
+
+}
