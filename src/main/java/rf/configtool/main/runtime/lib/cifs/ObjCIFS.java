7a74a14c76c2302829fcf20cac416ee9840587ea
Roar <rfo909@hotmail.com>
2022-04-10 17:06:02 +0200

    CIFS-lib changes

--- src/main/java/rf/configtool/main/runtime/lib/cifs/ObjCIFS.java
+++ src/main/java/rf/configtool/main/runtime/lib/cifs/ObjCIFS.java
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 */
 
-import java.security.MessageDigest;
 import java.util.List;
 import java.util.Properties;
 
@@ -43,7 +42,6 @@ public class ObjCIFS extends Obj {
 	
     public ObjCIFS () {
     	this.add(new FunctionCIFSFile());
-    	this.add(new FunctionCreateURL());
     	this.add(new FunctionCIFSContext());
     	this.add(new Function_Example());
     }
@@ -73,53 +71,20 @@ public class ObjCIFS extends Obj {
             return "CIFSFile";
         }
         public String getShortDesc() {
-            return "CIFSFile(url [, CIFSContext]) - create CIFSFile object from url starting with smb://";
+            return "CIFSFile(url, CIFSContext) - create CIFSFile object from url starting with smb://";
         }
         public Value callFunction (Ctx ctx, List<Value> params) throws Exception {
-        	String url=null;
-        	CIFSContext context=null;
-            if (params.size() >= 1) {
-            	url=getString("url",params,0);
-            } 
-            if (params.size() >= 2) {
-            	Obj obj=getObj("CIFCContext",params,1);
-            	context=((ObjCIFSContext) obj).getContext();
-            }
-            if (url==null) throw new Exception("Expected parameters url [, CIFSContext]");
+            if (params.size() != 2) throw new Exception("Expected parameters url, CIFSContext");
+            String url=getString("url",params,0);
+        	Obj obj=getObj("CIFCContext",params,1);
+        	CIFSContext context=((ObjCIFSContext) obj).getContext();
             
             if (!url.startsWith("smb://")) throw new Exception("Invalid url, should start with smb:// - got " + url);
+            
             return new ValueObj(new ObjCIFSFile(url,context));
         }
     } 
     
-    class FunctionCreateURL extends Function {
-        public String getName() {
-            return "createURL";
-        }
-        public String getShortDesc() {
-            return "createURL(domain?,username?,password?,hostport,path) - returns smb://... string";
-        }
-        public Value callFunction (Ctx ctx, List<Value> params) throws Exception {
-            if (params.size() != 5) throw new Exception("Expected parameters: domain?,username?,password?,hostport,path");
-            
-            String domain,username,password,hostport,path;
-            
-            domain=username=password=hostport=path=null;
-            
-            if (!(params.get(0) instanceof ValueNull)) domain=getString("domain",params,0);
-            if (!(params.get(1) instanceof ValueNull)) username=getString("username",params,1);
-            if (!(params.get(2) instanceof ValueNull)) password=getString("password",params,2);
-            
-            hostport=getString("hostport",params,3);
-            path=getString("path",params,4);
-            
-            
-            String url = createSmbUrl(domain, username, password, hostport, path);
-            return new ValueString(url);
-        }
-    }            
-
-
     class FunctionCIFSContext extends Function {
         public String getName() {
             return "CIFSContext";
@@ -149,24 +114,9 @@ public class ObjCIFS extends Obj {
             return "_Example";
         }
         public String getShortDesc() {
-            return "_Example() - display information about use";
+            return "_Example() - show example code";
         }
         private String[] data= {
-            "",
-            "Method 1: put all data into URL",
-            "-------------------------------",
-            "",
-            "domain=null",
-            "user='xxx'",
-            "password='yyy'",
-            "url=Lib.Util.CIFS.createURL(domain,user,password,'host','/share/path/file.txt')",
-            "file=Lib.Util.CIFS.CIFSFile(url)",
-            "",
-            "This method uses deprecated constructor for SmbFile() in ObjCIFSFile java class.",
-            "",
-            "",
-            "Method 2: use CIFSContext",
-            "-------------------------",
             "",
             "domain=null",
             "user='xxx'",
@@ -174,9 +124,6 @@ public class ObjCIFS extends Obj {
             "url='smb://host/share/path/file.txt'",
             "context=Lib.Util.CIFS.CIFSContext(domain,user,password)",
             "file=Lib.Util.CIFS.CIFSFile(url,context)",
-            "",
-            "This does NOT use deprecated code :-)",
-            "It also makes it safe to log the url, as it contains no secrets",
             "",
         };
         public Value callFunction (Ctx ctx, List<Value> params) throws Exception {
@@ -190,30 +137,27 @@ public class ObjCIFS extends Obj {
     } 
     
     
-	// -------------------------
-	// First implementation: embed all information into URL and create SmbFile from it alone.
-	//    new SmbFile(url) // deprecated
-	// -------------------------
-	
-	public static String createSmbUrl (String domain, String username, String password, String hostPort, String path) {
-		if (domain==null) domain=""; else domain=domain+";";
-		if (username==null) username="";
-		if (password==null) password=""; else password=":"+password;
-		
-		path=path.replace('\\', '/');
-		if (!path.startsWith("/")) path="/"+path;
+//	// -------------------------
+//	// First implementation: embed all information into URL and create SmbFile from it alone.
+//	//    new SmbFile(url) // deprecated
+//	// -------------------------
+//	
+//	public static String createSmbUrl (String domain, String username, String password, String hostPort, String path) {
+//		if (domain==null) domain=""; else domain=domain+";";
+//		if (username==null) username="";
+//		if (password==null) password=""; else password=":"+password;
+//		
+//		path=path.replace('\\', '/');
+//		if (!path.startsWith("/")) path="/"+path;
+//
+//		if((username+password).length() > 0) password=password+"@";
+//		
+//		return "smb://"+domain+username+password+hostPort+path;
+//	}
+//	
 
-		if((username+password).length() > 0) password=password+"@";
-		
-		return "smb://"+domain+username+password+hostPort+path;
-	}
-	
 
-	// -------------------------------------------------------------------------
-	// Second implementation
-	// -------------------------------------------------------------------------
-	
-	public static CIFSContext createCIFSContext (String domain, String username, String password) throws Exception {
+	private CIFSContext createCIFSContext (String domain, String username, String password) throws Exception {
 		NtlmPasswordAuthenticator auth = new NtlmPasswordAuthenticator(domain, username, password);
 		Properties p=new Properties();
 		
--- src/main/java/rf/configtool/main/runtime/lib/cifs/ObjCIFSFile.java
+++ src/main/java/rf/configtool/main/runtime/lib/cifs/ObjCIFSFile.java
@@ -1,40 +1,39 @@
 package rf.configtool.main.runtime.lib.cifs;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import jcifs.CIFSContext;
+import jcifs.CloseableIterator;
+import jcifs.SmbResource;
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
+import jcifs.smb.SmbFileOutputStream;
 import rf.configtool.main.Ctx;
 import rf.configtool.main.runtime.*;
+import rf.configtool.main.runtime.lib.ObjFile;
 import rf.configtool.util.TabUtil;
 
 public class ObjCIFSFile extends Obj {
 	
 	private String url;
+	private CIFSContext context;
 	private SmbFile smbFile;
 
     public ObjCIFSFile (String url, CIFSContext context) throws Exception {
     	this.url=url;
-    	if (context == null) {
-    		this.smbFile=new SmbFile(url);
-    	} else {
-    		this.smbFile=new SmbFile(url, context);
-    	}
+    	this.context=context;
+    	
+		this.smbFile=new SmbFile(url, context);
 
-    	this.add(new FunctionRead());
-    	this.add(new FunctionLength());
+		this.add(new FunctionLength());
     	this.add(new FunctionIsFile());
     	this.add(new FunctionIsDir());
     	this.add(new FunctionExists());
     	this.add(new FunctionList());
+    	this.add(new FunctionCopyFrom());
+    	this.add(new FunctionCopyTo());
 
     }
     
@@ -57,44 +56,7 @@ public class ObjCIFSFile extends Obj {
     public ColList getContentDescription() {
         return ColList.list().regular("CIFSFile");
     }
-    
-    private BufferedReader getBufferedReader() throws Exception {
-    	String encoding="ISO-8859-1";
-    	InputStream in=new SmbFileInputStream(smbFile);
-    	return new BufferedReader(new InputStreamReader(in, encoding));
-    }
-   
-    class FunctionRead extends Function {
-        public String getName() {
-            return "read";
-        }
-        public String getShortDesc() {
-            return "read() - return list of lines";
-        }
-        public Value callFunction (Ctx ctx, List<Value> params) throws Exception {
-            if (params.size() != 0) throw new Exception("Expected no parameters");
-            List<Value> result=new ArrayList<Value>();
-            BufferedReader br=null;
-            long lineNo=0;
-            try {
-                
-                br = getBufferedReader();
-                for (;;) {
-                    String line=br.readLine();
-                    lineNo++;
-                    if (line==null) break;
-                    
-                    String deTabbed=TabUtil.substituteTabs(line,4);
-                    result.add(new ValueString(deTabbed));  
-                }
-            } finally {
-                if (br != null) try {br.close();} catch (Exception ex) {};
-            }
-            return new ValueList(result);
-        }
-    }
-
-    
+  
     class FunctionLength extends Function {
         public String getName() {
             return "length";
@@ -157,12 +119,98 @@ public class ObjCIFSFile extends Obj {
         public Value callFunction (Ctx ctx, List<Value> params) throws Exception {
             if (params.size() != 0) throw new Exception("Expected no parameters");
             if (!smbFile.isDirectory()) throw new Exception("Not a directory");
+
+            
             List<Value> result=new ArrayList<Value>();
-            for (String s:smbFile.list()) {
+            SmbFile theFile=smbFile;
+            if (!url.endsWith("/")) {
+				// querying a directory, requires the path to end with "/" as the
+				// returned strings are "the last part of the url", and without 
+				// the "/" we get XXXa XXXb and so on, where XXX is current dir, and
+				// a,b is content inside.
+            	theFile=new SmbFile(url+"/", context);
+            }
+            for (String s:theFile.list()) {
+            	if (s.endsWith("/")) {
+            		// directories end with '/' - stripping it
+            		s=s.substring(0,s.length()-1);
+            	}
             	result.add(new ValueString(s));
             }
             return new ValueList(result);
         }
     }
 
+    
+    class FunctionCopyFrom extends Function {
+        public String getName() {
+            return "copyFrom";
+        }
+        public String getShortDesc() {
+            return "copyFrom(srcFile) - returns number of bytes copied";
+        }
+        public Value callFunction (Ctx ctx, List<Value> params) throws Exception {
+            if (params.size() != 1) throw new Exception("Expected srcFile parameter");
+            Obj obj=getObj("srcFile",params,0);
+            if (obj instanceof ObjFile) {
+            	File f=((ObjFile) obj).getFile();
+            		InputStream in=null;
+            		OutputStream out=null;
+            	try {
+                	in=new FileInputStream(f);
+                	out=new SmbFileOutputStream(smbFile);
+                	long count=copy(in,out);
+                	return new ValueInt(count);
+            	} finally {
+            		if (in != null) try {in.close();} catch (Exception ex) {};
+            		if (out != null) try {out.close();} catch (Exception ex) {};
+            	}
+            } else {
+            	throw new Exception("Expected srcFile parameter");
+            }
+        }
+    }
+
+    
+    class FunctionCopyTo extends Function {
+        public String getName() {
+            return "copyTo";
+        }
+        public String getShortDesc() {
+            return "copyTo(targetFile) - returns number of bytes copied";
+        }
+        public Value callFunction (Ctx ctx, List<Value> params) throws Exception {
+            if (params.size() != 1) throw new Exception("Expected targetFile parameter");
+            Obj obj=getObj("targetFile",params,0);
+            if (obj instanceof ObjFile) {
+            	File f=((ObjFile) obj).getFile();
+            		InputStream in=null;
+            		OutputStream out=null;
+            	try {
+                	in=new SmbFileInputStream(smbFile);
+                	out=new FileOutputStream(f);
+                	long count=copy(in,out);
+                	return new ValueInt(count);
+            	} finally {
+            		if (in != null) try {in.close();} catch (Exception ex) {};
+            		if (out != null) try {out.close();} catch (Exception ex) {};
+            	}
+            } else {
+            	throw new Exception("Expected targetFile parameter");
+            }
+        }
+    }
+
+    private long copy (InputStream in, OutputStream out) throws Exception {
+    	byte[] buf=new byte[64*1024];
+    	long count=0L;
+    	for (;;) {
+    		int i = in.read(buf);
+    		if (i<=0) break;
+    		out.write(buf,0,i);
+    		count +=i;
+    	}
+    	return count;
+    }
+    
 }
