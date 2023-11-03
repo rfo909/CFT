6a1bfc524b28d33d5b1b80a9729b9a763ad49ad6
Roar Foshaug <rfo909@hotmail.com>
2023-11-03 15:56:58 +0100

    v4.0.8

--- src/main/java/rf/configtool/main/runtime/lib/ObjRest.java
+++ src/main/java/rf/configtool/main/runtime/lib/ObjRest.java
@@ -16,6 +16,8 @@ public class ObjRest extends Obj {
     private String method="POST";
     private String basicAuthString;
     private String jsonData;
+    private boolean verbose = false;
+
 
 
     @Override
@@ -40,6 +42,7 @@ public class ObjRest extends Obj {
         this.add(new FunctionMethod());
         this.add(new FunctionBasicAuth());
         this.add(new FunctionJsonData());
+        this.add(new FunctionVerbose());
         this.add(new FunctionExecute());
 
     }
@@ -112,6 +115,23 @@ public class ObjRest extends Obj {
         }
     }
 
+    class FunctionVerbose extends Function {
+        public String getName() {
+            return "verbose";
+        }
+
+        public String getShortDesc() {
+            return "verbose() - enable verbose mode, returns self";
+        }
+
+        public Value callFunction(Ctx ctx, List<Value> params) throws Exception {
+            if (params.size() != 0) throw new Exception("Expected no parameters");
+            verbose=true;
+            log(ctx,"Rest: Enabling verbose mode");
+            return new ValueObj(self());
+        }
+    }
+
 
 
 
@@ -130,54 +150,72 @@ public class ObjRest extends Obj {
         }
     }
 
+    private void log (Ctx ctx, String msg) {
+        if (verbose) ctx.getStdio().println(msg);
+    }
 
 
     public ObjDict executeCall (Ctx ctx) throws Exception {
         ObjDict dict=new ObjDict();
 
         HttpURLConnection connection=null;
+        if (url==null) throw new Exception("No url");
+        if (method==null) throw new Exception("No method");
+
+        log(ctx,"---- executeCall() ----");
+
+        final String JSON = "application/json";
 
         try {
+            log(ctx,"Creating connection to " + url.toString());
             connection = (HttpURLConnection) url.openConnection();
 
+            log(ctx,"Setting method=" + method);
             connection.setRequestMethod(method);
 
             if (basicAuthString != null) {
+                log(ctx, "Setting authorization to basic: " + basicAuthString);
                 connection.setRequestProperty("Authorization", "Basic " + basicAuthString);
             }
             if (jsonData != null) {
-                connection.setRequestProperty("Content-Type", "application/json");
+                log(ctx,"Setting Content-Type: " + JSON);
+                connection.setRequestProperty("Content-Type", JSON);
             }
-            connection.setRequestProperty("Accepts", "application/json");
+            log(ctx,"Setting Accepts: " + JSON);
+            connection.setRequestProperty("Accepts", JSON);
 
             connection.setDoOutput(true);
             connection.setDoInput(true);
 
-            // Write the JSON data to the request body
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(jsonData.getBytes("UTF-8"));
-            //outputStream.close();
+            if (jsonData != null) {
+                log(ctx,"Writing jsonData: " + jsonData);
+                // Write the JSON data to the request body
+                OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(jsonData.getBytes("UTF-8"));
+                //outputStream.close();
+            }
 
             // Get the response code
             int responseCode = connection.getResponseCode();
             dict.set("responseCode", new ValueInt(responseCode));
+            log(ctx,"Got responseCode=" + responseCode);
 
-            if (responseCode % 100 == 2) {
+            //if (responseCode.toString().startsWith("2")) {
                 // Read the response body
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 StringBuilder response = new StringBuilder();
 
                 String line;
                 while ((line = reader.readLine()) != null) {
+                    log(ctx,line);
                     response.append(line);
                 }
 
                 // reader.close();
 
                 dict.set("result", new ValueString(response.toString()));
-            } else {
-                throw new Exception("Request failed. Response Code: " + responseCode);
-            }
+                log(ctx,"Got result: " + response.toString());
+            //}
             return dict;
         } finally {
             if (connection != null) try {connection.disconnect();} catch (Exception ex)  {};
