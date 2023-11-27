3533fc5d613ca2242d8f733fbf9b25ca1e097e34
Roar Foshaug <rfo909@hotmail.com>
2023-11-27 13:01:41 +0100

    v4.0.10

--- src/main/java/rf/configtool/main/runtime/lib/ObjRest.java
+++ src/main/java/rf/configtool/main/runtime/lib/ObjRest.java
@@ -1,27 +1,39 @@
 package rf.configtool.main.runtime.lib;
 
 import rf.configtool.main.Ctx;
+import rf.configtool.main.SourceException;
 import rf.configtool.main.runtime.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.http.HttpRequest;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 import java.net.URL;
+import java.util.UUID;
 
 public class ObjRest extends Obj {
 
     private String url;
     private String method="POST";
-    private String basicAuthString;
+    private String authString;
     private String jsonData;
     private boolean verbose = false;
 
+    private List<FileUpload> fileUploads = new ArrayList<FileUpload>();
 
-
+    class FileUpload {
+        public File file;
+        public String contentType;
+        public FileUpload (File file, String contentType) {
+            this.file=file;
+            this.contentType=contentType;
+        }
+    }
     @Override
     public boolean eq(Obj x) {
         return x == this;
@@ -43,9 +55,11 @@ public class ObjRest extends Obj {
         this.add(new FunctionUrl());
         this.add(new FunctionMethod());
         this.add(new FunctionBasicAuth());
+        this.add(new FunctionAuth());
         this.add(new FunctionJsonData());
         this.add(new FunctionVerbose());
         this.add(new FunctionExecute());
+        this.add(new FunctionMultipartFile());
 
     }
 
@@ -97,7 +111,22 @@ public class ObjRest extends Obj {
 
         public Value callFunction(Ctx ctx, List<Value> params) throws Exception {
             if (params.size() != 1) throw new Exception("Expected string parameter");
-            basicAuthString=getString("string", params, 0);
+            authString="Basic " + getString("string", params, 0);
+            return new ValueObj(self());
+        }
+    }
+    class FunctionAuth extends Function {
+        public String getName() {
+            return "auth";
+        }
+
+        public String getShortDesc() {
+            return "auth(string) - set Authorization string, returns self";
+        }
+
+        public Value callFunction(Ctx ctx, List<Value> params) throws Exception {
+            if (params.size() != 1) throw new Exception("Expected string parameter");
+            authString=getString("string", params, 0);
             return new ValueObj(self());
         }
     }
@@ -117,6 +146,33 @@ public class ObjRest extends Obj {
         }
     }
 
+    class FunctionMultipartFile extends Function {
+        public String getName() {
+            return "multipartFile";
+        }
+
+        public String getShortDesc() {
+            return "multipartFile(File, contentType?) - add multipart file (POST only) - returns self";
+        }
+
+        public Value callFunction(Ctx ctx, List<Value> params) throws Exception {
+            if (params.size() < 1 || params.size() > 2) throw new Exception("Expected File, contentType? parameter(s)");
+            Obj x=getObj("File", params, 0);
+            String contentType="application/octet-stream";
+
+            if (params.size()==2) {
+                contentType=getString("contentType", params, 1);
+            }
+            if (x instanceof ObjFile) {
+                File file=((ObjFile) x).getFile();
+
+                fileUploads.add(new FileUpload(file, contentType));
+            } else {
+                throw new Exception("Invalid File parameter");
+            }
+            return new ValueObj(self());
+        }
+    }
     class FunctionVerbose extends Function {
         public String getName() {
             return "verbose";
@@ -175,18 +231,25 @@ public class ObjRest extends Obj {
             log(ctx,"Setting method=" + method);
             connection.setRequestMethod(method);
 
-            if (basicAuthString != null) {
-                log(ctx, "Setting Authorization: Basic " + basicAuthString);
-                connection.setRequestProperty("Authorization", "Basic " + basicAuthString);
+            if (authString != null) {
+                log(ctx, "Setting Authorization: " + authString);
+                connection.setRequestProperty("Authorization", authString);
+            }
+
+            log(ctx, "Setting Accept: " + JSON);
+            connection.setRequestProperty("Accept", JSON);
+
+            // sanity check
+            if (jsonData != null && fileUploads.size() > 0) {
+                throw new Exception(getDesc() + ": Can not combine jsonData with multipartFile(s)");
+            }
+            if (fileUploads.size() > 0 && !method.equalsIgnoreCase("post")) {
+                throw new Exception(getDesc() + ": The multipartFile(s) require method POST");
             }
             if (jsonData != null) {
                 log(ctx,"Setting Content-Type: " + JSON);
                 connection.setRequestProperty("Content-Type", JSON);
-            }
-            log(ctx,"Setting Accept: " + JSON);
-            connection.setRequestProperty("Accept", JSON);
 
-            if (jsonData != null) {
                 connection.setDoOutput(true);
                 log(ctx,"Writing jsonData: " + jsonData);
                 // Write the JSON data to the request body
@@ -194,6 +257,45 @@ public class ObjRest extends Obj {
                 outputStream.write(jsonData.getBytes("UTF-8"));
                 outputStream.flush();
                 outputStream.close();
+            } else if (fileUploads.size() > 0 && method.equalsIgnoreCase("post")) {
+                final String boundary="----------994820020030051899";
+
+                connection.setDoOutput(true);
+                String mpart="multipart/form-data; boundary=" + boundary;
+                log(ctx, "Setting Content-Type: " + mpart);
+                connection.setRequestProperty("Content-Type", mpart);
+
+                OutputStream out=connection.getOutputStream();
+                PrintWriter writer = new PrintWriter(new OutputStreamWriter(out));
+
+                for (FileUpload fileUpload : fileUploads) {
+                    writer.append("--").append(boundary).append("\r\n");
+
+                    writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"").append(fileUpload.file.getName()).append("\"\r\n");
+                    writer.append("Content-Type: ").append(fileUpload.contentType).append("\r\n\r\n");
+                    writer.flush();
+
+                    FileInputStream fis=new FileInputStream(fileUpload.file);
+
+                    byte[] buf = new byte[8192];
+                    for (;;) {
+                        int count=fis.read(buf);
+                        if (count < 0) break;
+                        out.write(buf, 0, count);
+                    }
+                    out.flush();
+
+                    String md5 = hashMd5(fileUpload.file);
+                    writer.append("\r\n--").append(boundary).append("\r\n");
+                    writer.append("Content-Disposition: form-data; name=\"checksum\"\r\n\r\n");
+                    writer.append(md5);
+                    writer.flush();
+                }
+                // terminate multipart
+                writer.append("\r\n--").append(boundary).append("--\r\n");
+                writer.close();
+
+
             }
 
             // Get the response code
@@ -218,4 +320,37 @@ public class ObjRest extends Obj {
         }
 
     }
+
+    private static String hashMd5 (File file) throws Exception {
+        FileInputStream fis = null;
+
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        byte[] buf = new byte[4096];
+        try {
+            fis = new FileInputStream(file);
+            for (; ; ) {
+                int count = fis.read(buf);
+                if (count <= 0) break;
+                digest.update(buf, 0, count);
+            }
+        } finally {
+            if (fis != null) try {
+                fis.close();
+            } catch (Exception ex) {
+            }
+            ;
+        }
+
+        byte[] hash = digest.digest();
+
+        String digits = "0123456789abcdef";
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            byte b = hash[i];
+            sb.append(digits.charAt((b >> 4) & 0x0F));
+            sb.append(digits.charAt(b & 0x0F));
+        }
+        return sb.toString();
+    }
 }
+
